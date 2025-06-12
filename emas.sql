430b0c1cf315998f610ef9a35c285560b18344ed
Aril Ponco Nugroho <81026715+ArilPopon@users.noreply.github.com>
2025-06-12 19:39:08 +0700

    update sql

--- emas.sql
+++ emas.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 08 Jun 2025 pada 23.20
+-- Waktu pembuatan: 12 Jun 2025 pada 19.38
 -- Versi server: 10.4.32-MariaDB
 -- Versi PHP: 8.2.12
 
@@ -42,7 +42,7 @@ CREATE TABLE `appointments` (
 --
 
 INSERT INTO `appointments` (`id`, `user_id`, `appointment_date`, `appointment_time`, `note`, `status`, `created_at`) VALUES
-(4, 1, '2025-06-25', '15:00:00', 'Mau Konsultadi Perihal Pembuatan Perhiasan Custom', 'pending', '2025-06-08 04:55:36');
+(4, 1, '2025-06-25', '15:00:00', 'Mau Konsultadi Perihal Pembuatan Perhiasan Custom', 'confirmed', '2025-06-08 04:55:36');
 
 -- --------------------------------------------------------
 
@@ -147,30 +147,12 @@ CREATE TABLE `gold_transactions` (
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
 --
--- Struktur dari tabel `orders`
+-- Dumping data untuk tabel `gold_transactions`
 --
 
-CREATE TABLE `orders` (
-  `id` int(11) NOT NULL,
-  `user_id` int(11) DEFAULT NULL,
-  `total_price` decimal(15,2) DEFAULT NULL,
-  `status` enum('pending','paid','shipped','completed','cancelled') DEFAULT 'pending',
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `proof` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data untuk tabel `orders`
---
-
-INSERT INTO `orders` (`id`, `user_id`, `total_price`, `status`, `created_at`, `proof`) VALUES
-(1, 1, 46000000.00, 'paid', '2025-06-08 11:27:57', '1749382077_photo_2025-06-02_17-37-06.jpg'),
-(2, 1, 40500000.00, 'paid', '2025-06-08 11:33:00', '1749382380_ihidh.png'),
-(3, 1, 500000.00, 'pending', '2025-06-08 11:50:27', '1749383427_ahahahahumormeme.jpg'),
-(4, 1, 50300000.00, 'cancelled', '2025-06-08 16:19:38', '1749399578_memebinenalpha.jpg');
+INSERT INTO `gold_transactions` (`id`, `user_id`, `type`, `weight`, `price_per_gram`, `total_price`, `status`, `created_at`) VALUES
+(1, 1, 'sell', 1.00, 1900000.00, 1900000.00, 'pending', '2025-06-10 14:02:43');
 
 -- --------------------------------------------------------
 
@@ -197,7 +179,14 @@ INSERT INTO `order_items` (`id`, `order_id`, `product_id`, `quantity`, `price`) 
 (4, 2, 7, 1, 40000000.00),
 (5, 3, 8, 1, 500000.00),
 (6, 4, 3, 1, 300000.00),
-(7, 4, 5, 1, 50000000.00);
+(7, 4, 5, 1, 50000000.00),
+(8, 5, 10, 1, 500000.00),
+(9, 6, 7, 1, 40000000.00),
+(10, 7, 5, 1, 50000000.00),
+(11, 8, 3, 1, 300000.00),
+(12, 9, 6, 1, 600000.00),
+(13, 10, 8, 2, 500000.00),
+(14, 11, 2, 1, 10000000.00);
 
 -- --------------------------------------------------------
 
@@ -242,6 +231,41 @@ INSERT INTO `products` (`id`, `name`, `description`, `category_id`, `weight`, `m
 CREATE TABLE `transactions` (
   `id` int(11) NOT NULL,
   `user_id` int(11) DEFAULT NULL,
+  `total_price` decimal(15,2) DEFAULT NULL,
+  `status` enum('pending','paid','shipped','completed','cancelled') DEFAULT 'pending',
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `proof` varchar(255) DEFAULT NULL,
+  `paid_at` datetime DEFAULT NULL,
+  `shipped_at` datetime DEFAULT NULL,
+  `completed_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `transactions`
+--
+
+INSERT INTO `transactions` (`id`, `user_id`, `total_price`, `status`, `created_at`, `proof`, `paid_at`, `shipped_at`, `completed_at`) VALUES
+(1, 1, 46000000.00, 'paid', '2025-06-08 11:27:57', '1749382077_photo_2025-06-02_17-37-06.jpg', NULL, NULL, NULL),
+(2, 1, 40500000.00, 'completed', '2025-06-08 11:33:00', '1749382380_ihidh.png', NULL, NULL, NULL),
+(3, 1, 500000.00, 'shipped', '2025-06-08 11:50:27', '1749383427_ahahahahumormeme.jpg', NULL, NULL, NULL),
+(4, 1, 50300000.00, 'cancelled', '2025-06-08 16:19:38', '1749399578_memebinenalpha.jpg', NULL, NULL, NULL),
+(5, 1, 500000.00, 'cancelled', '2025-06-10 16:06:45', '1749571605_photo_2025-06-02_17-37-06.jpg', NULL, NULL, NULL),
+(6, 1, 40000000.00, 'paid', '2025-06-10 16:07:44', '1749571664_photo_2025-06-02_17-37-06.jpg', '2025-06-10 23:13:40', NULL, NULL),
+(7, 1, 50000000.00, 'paid', '2025-06-10 16:08:00', '1749571680_photo_2025-06-02_17-37-06.jpg', NULL, NULL, NULL),
+(8, 1, 300000.00, 'cancelled', '2025-06-10 16:15:41', '1749572141_photo_2025-06-02_17-37-06.jpg', '2025-06-10 23:15:52', NULL, NULL),
+(9, 1, 600000.00, 'completed', '2025-06-10 16:23:55', '1749572635_photo_2025-06-02_17-37-06.jpg', '2025-06-10 23:23:59', '2025-06-10 23:24:23', '2025-06-12 15:27:46'),
+(10, 1, 1000000.00, 'shipped', '2025-06-11 04:16:56', '1749615416_photo_2025-06-02_17-37-06.jpg', '2025-06-11 11:17:18', '2025-06-11 11:18:08', NULL),
+(11, 1, 10000000.00, 'pending', '2025-06-11 04:17:38', '1749615458_photo_2025-06-02_17-37-06.jpg', NULL, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `transactions_old`
+--
+
+CREATE TABLE `transactions_old` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
   `product_id` int(11) DEFAULT NULL,
   `quantity` int(11) DEFAULT NULL,
   `total_price` decimal(15,2) DEFAULT NULL,
@@ -251,10 +275,10 @@ CREATE TABLE `transactions` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data untuk tabel `transactions`
+-- Dumping data untuk tabel `transactions_old`
 --
 
-INSERT INTO `transactions` (`id`, `user_id`, `product_id`, `quantity`, `total_price`, `status`, `created_at`, `proof`) VALUES
+INSERT INTO `transactions_old` (`id`, `user_id`, `product_id`, `quantity`, `total_price`, `status`, `created_at`, `proof`) VALUES
 (1, 1, 9, 1, 6000000.00, 'pending', '2025-06-08 06:15:27', NULL),
 (2, 1, 9, 1, 6000000.00, 'pending', '2025-06-08 06:17:12', NULL),
 (3, 1, 8, 1, 500000.00, 'cancelled', '2025-06-08 06:19:53', NULL),
@@ -276,16 +300,18 @@ CREATE TABLE `users` (
   `password` varchar(255) DEFAULT NULL,
   `phone` varchar(20) DEFAULT NULL,
   `role` enum('customer','admin') DEFAULT 'customer',
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `address` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data untuk tabel `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `email`, `password`, `phone`, `role`, `created_at`) VALUES
-(1, 'Aril Ponco Nugroho', 'arilponconugroho@gmail.com', '$2y$10$IS0ew3rUehZgmieDeAHkTuXTtkTLaNLB2Hh9TLSakNProNZr2Ffjq', '0895619917517', 'customer', '2025-05-23 13:51:23'),
-(3, 'admin', 'admin@gmail.com', '$2y$10$mSwseMNBI4T0E4iBsRkeEeBTuZ8yy1dwJt3Pr9A0m/7oWJ.PePGFq', NULL, 'admin', '2025-05-23 14:56:56');
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `phone`, `role`, `created_at`, `address`) VALUES
+(1, 'Aril Ponco Nugroho', 'arilponconugroho@gmail.com', '$2y$10$IS0ew3rUehZgmieDeAHkTuXTtkTLaNLB2Hh9TLSakNProNZr2Ffjq', '0895619917517', 'customer', '2025-05-23 13:51:23', 'Jl. Rungkut Asri Rk V No 20, Medokan Ayu, Kec. Rungkut, Surabaya, Jawa Timur 60293'),
+(3, 'admin', 'admin@gmail.com', '$2y$10$mSwseMNBI4T0E4iBsRkeEeBTuZ8yy1dwJt3Pr9A0m/7oWJ.PePGFq', NULL, 'admin', '2025-05-23 14:56:56', NULL),
+(5, 'Gufron Abdurrahman', 'gufron@gmail.com', '$2y$10$YfoGUMwMcxncNdfI8TL7pehX.tj4sbEBUiiyxyoLmmGC.skudObju', '0867573416124', 'customer', '2025-06-12 09:02:51', 'VH2C+38C, jln.perumahan Graha sentosa,petak, RT.03/RW.06, Sawah Dan Kebun, Tambakromo, Kec. Cepu, Kabupaten Blora, Jawa Tengah 58315');
 
 --
 -- Indexes for dumped tables
@@ -333,13 +359,6 @@ ALTER TABLE `gold_transactions`
   ADD KEY `user_id` (`user_id`);
 
 --
--- Indeks untuk tabel `orders`
---
-ALTER TABLE `orders`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `user_id` (`user_id`);
-
---
 -- Indeks untuk tabel `order_items`
 --
 ALTER TABLE `order_items`
@@ -358,6 +377,13 @@ ALTER TABLE `products`
 -- Indeks untuk tabel `transactions`
 --
 ALTER TABLE `transactions`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indeks untuk tabel `transactions_old`
+--
+ALTER TABLE `transactions_old`
   ADD PRIMARY KEY (`id`),
   ADD KEY `user_id` (`user_id`),
   ADD KEY `product_id` (`product_id`);
@@ -383,7 +409,7 @@ ALTER TABLE `appointments`
 -- AUTO_INCREMENT untuk tabel `cart_items`
 --
 ALTER TABLE `cart_items`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- AUTO_INCREMENT untuk tabel `categories`
@@ -407,19 +433,13 @@ ALTER TABLE `gold`
 -- AUTO_INCREMENT untuk tabel `gold_transactions`
 --
 ALTER TABLE `gold_transactions`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT untuk tabel `orders`
---
-ALTER TABLE `orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT untuk tabel `order_items`
 --
 ALTER TABLE `order_items`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT untuk tabel `products`
@@ -431,13 +451,19 @@ ALTER TABLE `products`
 -- AUTO_INCREMENT untuk tabel `transactions`
 --
 ALTER TABLE `transactions`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT untuk tabel `transactions_old`
+--
+ALTER TABLE `transactions_old`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT untuk tabel `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
@@ -469,16 +495,10 @@ ALTER TABLE `gold_transactions`
   ADD CONSTRAINT `gold_transactions_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 
 --
--- Ketidakleluasaan untuk tabel `orders`
---
-ALTER TABLE `orders`
-  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
-
---
 -- Ketidakleluasaan untuk tabel `order_items`
 --
 ALTER TABLE `order_items`
-  ADD CONSTRAINT `order_items_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `order_items_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `transactions` (`id`) ON DELETE CASCADE,
   ADD CONSTRAINT `order_items_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`) ON DELETE CASCADE;
 
 --
@@ -491,24 +511,15 @@ ALTER TABLE `products`
 -- Ketidakleluasaan untuk tabel `transactions`
 --
 ALTER TABLE `transactions`
-  ADD CONSTRAINT `transactions_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
-  ADD CONSTRAINT `transactions_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`);
+  ADD CONSTRAINT `transactions_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `transactions_old`
+--
+ALTER TABLE `transactions_old`
+  ADD CONSTRAINT `transactions_old_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `transactions_old_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`);
 COMMIT;
-
---
--- Penambahan value gambar untuk table `gold_transaction`
---
-
-ALTER TABLE `gold_transactions` 
-  ADD COLUMN `photo` VARCHAR(255);
-
---
--- Penambahan value transaksi untuk tabel 'transaction'
---
-
-ALTER TABLE `transactions` ADD COLUMN `gold_transaction_id` INT DEFAULT NULL;
-
-
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
