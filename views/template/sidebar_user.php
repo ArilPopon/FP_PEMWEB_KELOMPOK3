67c475618d29def134899a8c48a8e0e0de57540f
ArilPopon <arilponconugroho@gmail.com>
2025-06-15 13:28:32 +0700

    fix : salah merge sidebar dashboard user

--- views/template/sidebar_user.php
+++ views/template/sidebar_user.php
@@ -4,5 +4,6 @@
 <a href="dashboard.php" class="<?= (basename($_SERVER['PHP_SELF']) === 'dashboard.php') ? 'active' : '' ?>">Ringkasan</a>
 <a href="riwayat.php" class="<?= (basename($_SERVER['PHP_SELF']) === 'riwayat.php') ? 'active' : '' ?>">Riwayat Transaksi</a>
 <a href="riwayat_booking.php" class="<?= (basename($_SERVER['PHP_SELF']) === 'riwayat_booking.php') ? 'active' : '' ?>">Riwayat Booking</a>
+<a href="dashboard_custom.php" class="<?= (basename($_SERVER['PHP_SELF']) === 'dashboard_custom.php') ? 'active' : '' ?>">Custom</a>
 <a href="pengiriman.php" class="<?= (basename($_SERVER['PHP_SELF']) === 'pengiriman.php') ? 'active' : '' ?>">Pengiriman</a>
 <a href="profil.php" class="<?= (basename($_SERVER['PHP_SELF']) === 'profil.php') ? 'active' : '' ?>">Profil</a>
